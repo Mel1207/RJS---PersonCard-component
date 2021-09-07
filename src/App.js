71e493c15e1daa329e6f00b6d619e9f6de2d849a
Mel <meljohnpualon@gmail.com>
2021-09-08 05:11:40 +0800

    added dynamic person card with props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './index.css';
 
+const Person = ({img, name, job}) => {
+  const url = (`https://randomuser.me/api/portraits/men/${img}.jpg`)
+
+  return (
+    <div>
+      <img src={url} alt="Person-1" />
+      <h5>{name}</h5>
+      <h4>{job}</h4>
+    </div>
+  );
+}
+
 
 function PersonCard() {
   return (
     <div className="App">
-      <h1>hey theree!</h1>
+      <Person img="45" name="John" job="developer"/>
+      <Person img="55" name="Mel" job="Frontend"/>
+      <Person img="70" name="Jax" job="Fullstack"/>
       
     </div>
   );
